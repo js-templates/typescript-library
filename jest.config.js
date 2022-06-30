689d1c95597081d1a43de3523199d4416d290585
Evgeny Zakharov <risenforces@gmail.com>
2022-07-01 01:47:49 +0300

    feat: add jest tsconfig paths alias support

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   passWithNoTests: true,
   snapshotSerializers: ['jest-serializer-path'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}),
 }
